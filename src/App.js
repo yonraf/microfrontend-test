d250c2aa5c632d21c91d8cb51a0892df2457851d
Yonus Rafik <yora@gfforsikring.dk>
2022-12-05 13:22:19 +0100

    first draft

--- src/App.js
+++ src/App.js
@@ -1,23 +1,24 @@
-import logo from './logo.svg';
 import './App.css';
+import MicroFrontend from './MicroFrontend';
+
+
+const {
+  REACT_APP_BROWSE_HOST: browseHost,
+  REACT_APP_RESTAURANT_HOST: restaurantHost,
+} = process.env;
+
+const Browse = ({ history }) => (
+  <MicroFrontend history={history} host={browseHost} name="Browse" />
+);
+const Restaurant = ({ history }) => (
+  <MicroFrontend history={history} host={restaurantHost} name="Restaurant" />
+);
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <Browse/>
+      <Restaurant/>
     </div>
   );
 }
